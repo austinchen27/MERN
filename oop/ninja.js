85c2cbb84447ab4eafdb816eb62bd976a9c48457
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-11 15:03:22 -0800

    kerm

--- oop/ninja.js
+++ oop/ninja.js
@@ -1,23 +1,23 @@
 class Ninja {
-  constructor(name) {
-      this.name = name;
-      this.health = 100;
-      this.speed = 3;
-      this.strength = 3;
-  }
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
 
-  sayName() {
-      console.log(`My ninja name is ${this.name}`);
-  }
+    sayName() {
+        console.log(`My ninja name is ${this.name}`);
+    }
 
-  showStats() {
-      console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
-  }
+    showStats() {
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+    }
 
-  drinkSake() {
-      this.health += 10;
-      console.log(`${this.name} drank sake and gained 10 health. Current health: ${this.health}`);
-  }
+    drinkSake() {
+        this.health += 10;
+        console.log(`${this.name} drank sake and gained 10 health. Current health: ${this.health}`);
+    }
 }
 
 const ninja1 = new Ninja("Snake Eyes");
@@ -27,18 +27,18 @@ ninja1.showStats(); // Output: "Name: Snake Eyes, Health: 100, Speed: 3, Strengt
 ninja1.drinkSake(); // Output: "Snake Eyes drank sake and gained 10 health. Current health: 110"
 
 class Sensei extends Ninja {
-  constructor(name) {
-      super(name);
-      this.health = 200;
-      this.speed = 10;
-      this.strength = 10;
-      this.wisdom = 10;
-  }
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
 
-  speakWisdom() {
-      super.drinkSake();
-      console.log("Programming is a way of life.");
-  }
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Programming is a way of life.");
+    }
 }
 
 const sensei1 = new Sensei("Austin");
