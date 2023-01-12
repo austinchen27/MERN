67a01b23a5195a12e1ee87e7215408cbdbebe564
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-12 12:54:52 -0800

    k

---
+++ react/prop-it-up/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import PersonCard from './components/PersonCard';
+
+function App() {
+
+  return (
+    <>
+      <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black" />
+      <PersonCard firstName="Smith" lastName="John" age="88" hairColor="Brown" />
+      <PersonCard firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown" />
+      <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown" />
+    </>
+  );
+}
+
+export default App;
