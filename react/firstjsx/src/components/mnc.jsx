95c09cfd902d0670e988fba8b67e9279fa6026e8
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-11 21:58:37 -0800

    k

---
+++ react/firstjsx/src/components/mnc.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class mnc extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo</h1>
+        <h2>Things I need to do</h2>
+
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default mnc;
