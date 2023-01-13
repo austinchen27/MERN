7cb0049fcc6de736df1283e71cc5d2a6c9bbbde7
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-12 21:37:40 -0800

    k

--- react/putting-it-together/src/App.js
+++ react/putting-it-together/src/App.js
@@ -4,11 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import PersonCard from './components/PersonCard';
 
+var peopleArr =[
+  {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
+  {"firstName":"John", "lastName":"Smith", "age":88, "hairColor":"Brown"},
+  {"firstName":"Millard", "lastName":"Fillmore", "age":50, "hairColor":"Brown"},
+  {"firstName":"Maria", "lastName":"Smith", "age":62, "hairColor":"Brown"}
+]
+
 function App() {
   return (
     <>
-      <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
-      <PersonCard firstName={"Smith"} lastName={"John"} age={"88"} hairColor={"Brown"} />
+      {peopleArr.map(person => {
+        return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
+      }
+        
+        )}
     </>
   );
 }
