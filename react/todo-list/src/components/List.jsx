b480621ab07a3533d894532c1b11421e7006e396
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-19 17:36:19 -0800

    k

--- react/todo-list/src/components/List.jsx
+++ react/todo-list/src/components/List.jsx
@@ -7,20 +7,14 @@ const List = (props) => {
     if (props.todo[e.target.value].completed === false) {
       props.todo[e.target.value].completed = true
     } else {
-      props.todo[e.target.value].copmleted = false
+      props.todo[e.target.value].completed = false
     }
     props.setToDo([...props.todo]);
   }
 
   const deleteTodo = (e) => {
-    let todoList = props.todo;
-    let newList = []
-    for (let i = 0; i < todoList.length; i++) {
-      if (e.target.value !== i) {
-        newList.push(todoList[i])
-      }
-    }
-    props.setToDo(newList);
+    props.todo.splice(e.target.value, 1)
+    props.setToDo([...props.todo])
   }
 
   return (
@@ -28,7 +22,7 @@ const List = (props) => {
       {
         props.todo.map((todo, i) =>
           <div key={i}>
-            <span className={todo.completed ? "done" : "a"}>{todo.text}</span>
+            <span className={todo.completed ? "done" : "incomplete"}>{todo.text}</span>
             <input type="checkbox" onClick={strikeThrough} value={i} />
             <button onClick={deleteTodo} value={i}>Delete</button>
             {todo.completed}
