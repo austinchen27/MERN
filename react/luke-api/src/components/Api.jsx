bf3ba7d36f41fabbd7ed14163332d6fdb2dbf9df
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-24 11:19:55 -0800

    k

--- react/luke-api/src/components/Api.jsx
+++ react/luke-api/src/components/Api.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router';
-import { useNavigate } from 'react-router-dom';
 
 const Api = (props) => {
   const [data, setData] = useState([]);
