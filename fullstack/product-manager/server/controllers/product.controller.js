cf10537ecc6f1afc112178f1abca9a7ad526b7f2
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2023-01-28 00:54:43 -0800

    k

---
+++ fullstack/product-manager/server/controllers/product.controller.js
@@ -0,0 +1,18 @@
+const { Product } = require('../models/product.model');
+
+module.exports.createProduct = (req, res) => {
+  const { title, price, description } = req.body;
+  Product.create({
+    title,
+    price,
+    description
+  })
+    .then(product => res.json(product))
+    .catch(err => res.json(err))
+}
+
+module.exports.oneProduct = (req, res) => {
+  Product.findById({ _id: req.params.id })
+    .then(product => res.json(product))
+    .catch(err => res.json(err))
+}
